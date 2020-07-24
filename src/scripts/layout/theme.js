02ae2c64f57756b7f9a54f7b89b192a5b85650d5
arunumusic <arunkumar.shukla@umusic.com>
2020-07-24 15:18:59 +0530

    js and css and responsive fixed

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -1,15 +1,16 @@
-import 'lazysizes/plugins/object-fit/ls.object-fit';
-import 'lazysizes/plugins/parent-fit/ls.parent-fit';
-import 'lazysizes/plugins/rias/ls.rias';
-import 'lazysizes/plugins/bgset/ls.bgset';
-import 'lazysizes';
-import 'lazysizes/plugins/respimg/ls.respimg';
+import "lazysizes/plugins/object-fit/ls.object-fit";
+import "lazysizes/plugins/parent-fit/ls.parent-fit";
+import "lazysizes/plugins/rias/ls.rias";
+import "lazysizes/plugins/bgset/ls.bgset";
+import "lazysizes";
+import "lazysizes/plugins/respimg/ls.respimg";
+import "jquery";
 
-import '../../styles/theme.scss';
-import '../../styles/theme.scss.liquid';
+import "../../styles/theme.scss";
+import "../../styles/theme.scss.liquid";
 
-import {focusHash, bindInPageLinks} from '@shopify/theme-a11y';
-import {cookiesEnabled} from '@shopify/theme-cart';
+import { focusHash, bindInPageLinks } from "@shopify/theme-a11y";
+import { cookiesEnabled } from "@shopify/theme-cart";
 
 // Common a11y fixes
 focusHash();
@@ -18,7 +19,7 @@ bindInPageLinks();
 // Apply a specific class to the html element for browser support of cookies.
 if (cookiesEnabled()) {
   document.documentElement.className = document.documentElement.className.replace(
-    'supports-no-cookies',
-    'supports-cookies',
+    "supports-no-cookies",
+    "supports-cookies"
   );
 }
--- src/scripts/templates/index.js
+++ src/scripts/templates/index.js
@@ -1,4 +1,34 @@
-import {load} from '@shopify/theme-sections';
-import '../sections/product';
+import { load } from "@shopify/theme-sections";
+import "../sections/product";
 
-load('*');
+load("*");
+
+const acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = `${panel.scrollHeight}px`;
+    }
+  });
+}
+
+const navbar = document.getElementById("header");
+
+const onScroll = () => {
+  const scroll = document.documentElement.scrollTop;
+  if (screen.width <= 769) {
+    if (scroll > 0) {
+      navbar.classList.add("header-bg");
+    } else {
+      navbar.classList.remove("header-bg");
+    }
+  }
+};
+
+window.addEventListener("scroll", onScroll);
